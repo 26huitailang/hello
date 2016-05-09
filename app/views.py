b72b80ced227629f9beb3957677fbf597f3c12f4
26huitailang <26huitailang@gmail.com>
2016-05-09 17:56:32 +0800

    database unsolved

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,15 @@ def index():
                             title='',
                             user=user,
                             posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                            title=u'登录',
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])

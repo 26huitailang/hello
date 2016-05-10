351b67685264876d46a4be174733fcaf364856f2
26huitailang <26huitailang@gmail.com>
2016-05-10 18:23:43 +0800

    login part

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')  # read config and use it
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models

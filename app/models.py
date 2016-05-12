bb370d2beb6506274c04498d22b61dd83b8a1032
26huitailang <26huitailang@gmail.com>
2016-05-12 17:46:01 +0800

    sql update

--- app/models.py
+++ app/models.py
@@ -8,7 +8,6 @@ if enable_search:
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), nullable=False, index=True, unique=True)

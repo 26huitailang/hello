6328ced1550ee9c2e8dd0f92fb4c501808e3d566
26huitailang <26huitailang@gmail.com>
2016-05-12 18:14:33 +0800

    del openid

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ def load_user(id):
 
 @app.route('/')
 @app.route('/index')
-@login_required
+# @login_required
 def index():
     user = g.user
     posts = [  # fake array of posts
@@ -35,39 +35,39 @@ def index():
 def before_request():
     g.user = current_user
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                            title=u'登录',
-                            form=form,
-                            providers=app.config['OPENID_PROVIDERS'])
+# @app.route('/login', methods=['GET', 'POST'])
+# @oid.loginhandler
+# def login():
+#     if g.user is not None and g.user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         session['remember_me'] = form.remember_me.data
+#         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+#     return render_template('login.html',
+#                             title=u'登录',
+#                             form=form,
+#                             providers=app.config['OPENID_PROVIDERS'])
 
-@oid.after_login
-def after_login(resp):  # The resp argument passed to the after_login function contains information returned by the OpenID provider.
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember = remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
+# @oid.after_login
+# def after_login(resp):  # The resp argument passed to the after_login function contains information returned by the OpenID provider.
+#     if resp.email is None or resp.email == "":
+#         flash('Invalid login. Please try again.')
+#         return redirect(url_for('login'))
+#     user = User.query.filter_by(email=resp.email).first()
+#     if user is None:
+#         nickname = resp.nickname
+#         if nickname is None or nickname == "":
+#             nickname = resp.email.split('@')[0]
+#         user = User(nickname=nickname, email=resp.email)
+#         db.session.add(user)
+#         db.session.commit()
+#     remember_me = False
+#     if 'remember_me' in session:
+#         remember_me = session['remember_me']
+#         session.pop('remember_me', None)
+#     login_user(user, remember = remember_me)
+#     return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/user/<nickname>')
 @login_required
